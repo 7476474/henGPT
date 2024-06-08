0f99bad97303e13b77be2753cf7aa641b2bfe8c2
7476474 <134752269+7476474@users.noreply.github.com>
2024-06-09 01:38:44 +0800

    Update index.tsx

--- src/components/BrandWatermark/index.tsx
+++ src/components/BrandWatermark/index.tsx
@@ -1,8 +1,4 @@
-'use client';
-
-import { Logo } from '@lobehub/ui';
 import { createStyles } from 'antd-style';
-import Link from 'next/link';
 import { memo } from 'react';
 import { Flexbox, FlexboxProps } from 'react-layout-kit';
 
@@ -28,7 +24,7 @@ const BrandWatermark = memo<Omit<FlexboxProps, 'children'>>(({ style, ...rest })
       style={{ color: theme.colorTextDescription, fontSize: 12, ...style }}
       {...rest}
     >
-      <span>前往 <strong><a className={styles.logoLink} href="https://chenhen.rf.gd" target="_blank" rel="noopener noreferrer">chenHen社区</a></strong></span>
+      <span>前往 <strong><a href="https://chenhen.rf.gd" target="_blank" rel="noopener noreferrer">chenHen社区</a></strong></span>
     </Flexbox>
   );
 });
